ac0b2993f701851f3766286d3f7f22da41a96f5a
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-03-14 01:32:24 +0900

    Feat: add echo handler

---
+++ pkg/handlers/echo/echo.go
@@ -0,0 +1,42 @@
+package echo
+
+import (
+	"context"
+	"regexp"
+
+	"github.com/genkami/go-slack-event-router/appmention"
+	routererrors "github.com/genkami/go-slack-event-router/errors"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+
+	"github.com/genkami/daemon3/pkg/framework"
+)
+
+type Handler struct {
+	framework *framework.Framework
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) Register(f *framework.Framework) error {
+	h.framework = f
+	f.EventRouter.OnAppMention(appmention.HandlerFunc(h.HandleEcho), appmention.TextRegexp(regexp.MustCompile(`\becho\b`)))
+	return nil
+}
+
+var echoPattern = regexp.MustCompile(`\becho[[:space:]]+(.+)`)
+
+func (h *Handler) HandleEcho(e *slackevents.AppMentionEvent) error {
+	groups := echoPattern.FindStringSubmatch(e.Text)
+	if len(groups) < 2 {
+		return routererrors.NotInterested
+	}
+	arg := groups[1]
+	ctx := context.TODO()
+	if _, _, err := h.framework.Client.PostMessageContext(ctx, e.Channel, slack.MsgOptionText(arg, true)); err != nil {
+		return err
+	}
+	return nil
+}
