ac5a525d3ce7a8434389218397df3bbd4c46103a
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-03-14 15:31:40 +0900

    Feat: fetch secrets from Google Cloud Secret Manager

--- cmd/daemon3/main.go
+++ cmd/daemon3/main.go
@@ -1,54 +1,74 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/genkami/daemon3/pkg/framework"
-	"github.com/genkami/daemon3/pkg/handlers/echo"
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	eventrouter "github.com/genkami/go-slack-event-router"
 	"github.com/genkami/go-slack-event-router/interactionrouter"
 	"github.com/slack-go/slack"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+
+	"github.com/genkami/daemon3/pkg/framework"
+	"github.com/genkami/daemon3/pkg/handlers/echo"
 )
 
 type Params struct {
-	BindAddr      string
-	SigningSecret string
-	BotToken      string
+	BindAddr               string
+	SlackSecretVersionName string
+	Slack                  SlackSecret
 }
 
-func (p *Params) ParseFlags() {
+type SlackSecret struct {
+	SigningSecret string `json:"signingSecret"`
+	BotToken      string `json:"botToken"`
+}
+
+func (p *Params) Setup() {
 	flag.StringVar(&p.BindAddr, "addr", "", "bind address")
+	flag.StringVar(&p.SlackSecretVersionName, "slack-secret-version-name", "", "the name of the secret version of the Slack secrets")
 	flag.Parse()
 
-	p.SigningSecret = os.Getenv("SLACK_SIGNING_SECRET")
-	if p.SigningSecret == "" {
-		fmt.Fprintln(os.Stderr, "SLACK_SIGNING_SECRET must be set")
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to initialize secret manager client: %v\n", err.Error())
 		os.Exit(1)
 	}
-	p.BotToken = os.Getenv("SLACK_BOT_TOKEN")
-	if p.BotToken == "" {
-		fmt.Fprintln(os.Stderr, "SLACK_BOT_TOKEN must be set")
+
+	resp, err := client.AccessSecretVersion(ctx, &secretmanagerpb.AccessSecretVersionRequest{
+		Name: p.SlackSecretVersionName,
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to get signing secret: %s\n", err.Error())
+		os.Exit(1)
+	}
+	err = json.Unmarshal(resp.Payload.Data, &p.Slack)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to parse slack secrets: %s\n", err.Error())
 		os.Exit(1)
 	}
 }
 
 func main() {
 	var p Params
-	p.ParseFlags()
-	eventRouter, err := eventrouter.New(eventrouter.WithSigningSecret(p.SigningSecret))
+	p.Setup()
+	eventRouter, err := eventrouter.New(eventrouter.WithSigningSecret(p.Slack.SigningSecret))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to initialize event router: %s\n", err.Error())
 		os.Exit(1)
 	}
-	interactionRouter, err := interactionrouter.New(interactionrouter.WithSigningSecret(p.SigningSecret))
+	interactionRouter, err := interactionrouter.New(interactionrouter.WithSigningSecret(p.Slack.SigningSecret))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to initialize interaction router: %s\n", err.Error())
 		os.Exit(1)
 	}
-	client := slack.New(p.BotToken)
+	client := slack.New(p.Slack.BotToken)
 
 	framework := &framework.Framework{
 		EventRouter:       eventRouter,
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/genkami/daemon3
 go 1.16
 
 require (
+	cloud.google.com/go v0.79.0 // indirect
 	github.com/genkami/go-slack-event-router v0.2.0 // indirect
 	github.com/slack-go/slack v0.8.1 // indirect
+	google.golang.org/genproto v0.0.0-20210312152112-fc591d9ea70f // indirect
 )
